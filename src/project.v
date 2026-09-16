176acfdad5cb31a2470581c01abf4461b1e71555
gyx <gxia@uwaterloo.ca>
2026-09-16 14:59:36 -0400

    test

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
+    output reg [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
@@ -17,11 +17,15 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) uo_out <= ui_in;
+    else uo_out <= uo_out + 1;
+  end
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, 1'b0};
 
 endmodule
