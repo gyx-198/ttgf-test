a11e2fe947ca9e2863c862fcac259b474d82516b
gyx <gxia@uwaterloo.ca>
2026-09-16 15:50:41 -0400

    tests passed

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
-    output reg [7:0] uo_out,   // Dedicated outputs
+    output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
@@ -16,13 +16,16 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  reg [7:0] count;
+
   // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out = count;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) uo_out <= ui_in;
-    else uo_out <= uo_out + 1;
+    if (!rst_n) count <= ui_in;
+    else count <= count + 1;
   end
 
   // List all unused inputs to prevent warnings
--- test/test.py
+++ test/test.py
@@ -26,7 +26,7 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
+    dut.ui_in.value = 240
     dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
@@ -38,3 +38,31 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+    for i in range(4):
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == i+1, f"Expected {i+1}, got {dut.uo_out.value}"
+    
+    dut._log.info("Reset")
+    dut.rst_n.value = 0
+    for i in range(10):
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == dut.ui_in.value, f"Expected {dut.ui_in.value}, got {dut.uo_out.value}"
+    dut.rst_n.value = 1
+
+    # dut._log.info(dut.uo_out.value)
+    # await ClockCycles(dut.clk, 1)
+    # dut._log.info(dut.uo_out.value)
+    # await ClockCycles(dut.clk, 30)
+    # dut._log.info(dut.uo_out.value)
+    
+    for i in range(16):
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == int(dut.ui_in.value)+i, f"Expected {int(dut.ui_in.value)+i}, got {dut.uo_out.value}"
+    
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0, f"Expected 0, got {dut.uo_out.value}"
+
+    for i in range(4):
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == i+1, f"Expected {i+1}, got {dut.uo_out.value}"
+    
